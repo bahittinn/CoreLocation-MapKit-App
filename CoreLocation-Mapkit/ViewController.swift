48a36a2203bedbffb464985a617990d30747984e
bahittinn <bahittin84@gmail.com>
2023-09-14 21:49:19 +0300

    project setup

---
+++ CoreLocation-Mapkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreLocation-Mapkit
+//
+//  Created by Bahittin on 14.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
