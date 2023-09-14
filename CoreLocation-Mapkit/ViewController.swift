bc10984b0462392afb2abc6347b04407595afdee
bahittinn <bahittin84@gmail.com>
2023-09-14 22:15:48 +0300

    mapkit and pin

--- CoreLocation-Mapkit/ViewController.swift
+++ CoreLocation-Mapkit/ViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 import CoreLocation
-
+import MapKit
 class ViewController: UIViewController {
 
     var locationManager: CLLocationManager  = CLLocationManager()
     
+    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var enlemLabel: UILabel!
     @IBOutlet weak var boylamLabel: UILabel!
     @IBOutlet weak var hizLabel: UILabel!
@@ -27,6 +28,22 @@ class ViewController: UIViewController {
         
         locationManager.startUpdatingLocation()
         
+        
+        let konum = CLLocationCoordinate2D(latitude: 37.0139023, longitude: 35.2945878)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        let bolge = MKCoordinateRegion(center: konum, span: span)
+        
+        mapView.setRegion(bolge, animated: true)
+        
+        let pin = MKPointAnnotation()
+        
+        pin.coordinate = konum
+        pin.title = "Adana Ev"
+        pin.subtitle = "Altaşlık"
+        mapView.addAnnotation(pin)
+        
     }
 }
 
@@ -37,6 +54,10 @@ extension ViewController: CLLocationManagerDelegate {
         enlemLabel.text = "Enlem: \(sonKonum.coordinate.latitude)"
         boylamLabel.text = "Boylam: \(sonKonum.coordinate.longitude)"
         hizLabel.text = "Hiz: \(sonKonum.speed)"
+        
+        
+        
+        
     }
 }
 
